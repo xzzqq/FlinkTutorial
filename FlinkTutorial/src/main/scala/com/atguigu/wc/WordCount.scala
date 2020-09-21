51584fb0924bb962b80b7372ef8e3dfdf0453d3e
xzzqq <43594135+xzzqq@users.noreply.github.com>
2020-09-21 16:54:34 +0800

    Add files via upload

---
+++ FlinkTutorial/src/main/scala/com/atguigu/wc/WordCount.scala
@@ -0,0 +1,11 @@
+package com.atguigu.wc
+
+import org.apache.flink.api.scala.ExecutionEnvironment
+
+//批处理的word count
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    // 创建一个批处理的执行环境
+    val env = ExecutionEnvironment.getExecutionEnvironment
+  }
+}
